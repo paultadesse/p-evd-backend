44a20d564bba415f9859c36cf00faf78b5d0ad0f
Paulos Tadesse <paulosta952@gmail.com>
2022-02-11 18:03:10 +0300

    :bug: fix a bug forgot to add HasApiTokens Trait

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,10 +6,11 @@ use App\Models\SuperAdmin;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $guard = 'admin';
 
