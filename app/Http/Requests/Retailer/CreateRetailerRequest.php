b7657ec895db6e3966c1edbe653794bf87582cb4
Paulos Tadesse <paulosta952@gmail.com>
2022-02-16 20:30:09 +0300

    :zap: retailer request created

---
+++ app/Http/Requests/Retailer/CreateRetailerRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Retailer;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateRetailerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        //later change this return statment to check auth guard [ it must be only retailer-api ]
+        // return auth()->getDefaultDriver() == 'retailer-api';
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'username' => 'required',
+            'email' => 'required|email|unique:retailers',
+            'password' => 'required',
+        ];
+    }
+
+    //override the faildValidation method of this form request class...
+    protected function failedValidation(Validator $validator) 
+    { 
+        throw new HttpResponseException(response()->json($validator->errors(), 422)); 
+    }
+}
